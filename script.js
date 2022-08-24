8385336dff11e106fd6c30d3f18af75181344af5
Your NameNitroceliulioze <aksionova.diana@gmail.com>
2022-08-25 00:27:34 +0300

    changed task 5

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ function sum (a, b) {
     return a + b;
 }
 
-// 5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
-
-function takeTwoArrays(arr1, arr2) {
-    const mergeArr = arr1.concat(arr2)
-    return mergeArr
-}
-
 // 3. Write a function that takes a number (a) as argument Split a into its individual digits and return them in an array.
 
 function digits(a) {
@@ -18,20 +11,6 @@ function digits(a) {
 };
 
 //just copied from stackoverflow
-
-
-
-
-// others I didn't do, but wanted to have the solutions. 
-
-
-
-// 2. Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
-
-function myfunc(str) {
-    // console.log(str.slice(0, -3))
-    return str.slice(0, -3)
-}
 
 
 
@@ -48,6 +27,29 @@ returnLastElem(myArray, 3);
 
 
 
+
+// others I didn't do, but wanted to have the solutions. 
+
+
+
+// 2. Write a function that takes a string as an argument. Extract the last 3 characters from the string. Return the result
+
+function myfunc(str) {
+    // console.log(str.slice(0, -3))
+    return str.slice(0, -3)
+}
+
+
+// 5. Write a function that takes two arrays as arguments Merge both arrays and remove duplicate values Sort the merge result in ascending order Return the resulting array
+
+function takeTwoArrays(arr1, arr2) {
+    const mergeArr = arr1.concat(arr2).sort()
+    const noDubs = [...new Set(mergeArr)]
+    // console.log(noDubs)
+    return noDubs
+}
+  
+  takeTwoArrays([3, 2, 1, 3, 3], ['a', 'r', 'd'])
 
 // 6. Write a function that takes an object (a) and a string (b) as argument  Return true if the object has a property with key 'b' Return false otherwise
 
